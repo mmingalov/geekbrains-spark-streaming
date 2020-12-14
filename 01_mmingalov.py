7d1203cf0025aeb79cf8814432b2b3fcc501ac5a
mmingalov <mmingalov@gmail.com>
2020-12-14 20:06:08 +1000

    03+

--- 01_mmingalov.py
+++ 01_mmingalov.py
@@ -4,6 +4,7 @@ from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql import functions as F
 from pyspark.sql.types import StructType, StringType
 
+#если работаем из консоли -- то эта строчка не нужна
 spark = SparkSession.builder.appName("mmingalov_spark").getOrCreate()
 
 ######RATE SOURCE
